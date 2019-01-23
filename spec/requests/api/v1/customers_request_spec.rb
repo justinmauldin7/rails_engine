347aac6e7e9063419c1f28cbfa56ccf0eba22954
Justin Mauldin <justin.mauldin7@gmail.com>
2019-01-23 14:53:34 -0700

    Adds controller and json to get customers index test to pass.

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -4,8 +4,12 @@ describe "Customers API" do
   it "sends a list of customers" do
     create_list(:customer, 3)
 
-    get 'api/v1/customers'
+    get '/api/v1/customers'
 
     expect(response).to be_successful
+
+    customers = JSON.parse(response.body)
+
+    expect(customers.count).to eq(3)
   end
 end
