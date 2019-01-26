a7beca9d11385341916c0995e825463a7bab9966
Justin Mauldin <justin.mauldin7@gmail.com>
2019-01-26 14:25:32 -0700

    Adds request test for x most revenue method and updates transactions factory to all create with success as status.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,27 +2,38 @@ require 'rails_helper'
 
 describe "Merchants API" do
   before :each do
-    @merchant_1 = create(:merchant)
-    @merchant_2 = create(:merchant)
-    @merchant_3 = create(:merchant)
+    @customer_1 = create(:customer, first_name: "One")
+    @customer_2 = create(:customer, first_name: "Two")
+    @customer_3 = create(:customer, first_name: "Three")
 
-    @customer_1 = create(:customer)
+    @merchant_1 = create(:merchant, name: "Merchant One")
+    @merchant_2 = create(:merchant, name: "Merchant Two")
+    @merchant_3 = create(:merchant, name: "Merchant Three")
 
-    @invoice_1 = create(:invoice, customer: @customer_1, merchant: @merchant_1)
-    @invoice_2 = create(:invoice, customer: @customer_1, merchant: @merchant_2)
-    @invoice_3 = create(:invoice, customer: @customer_1, merchant: @merchant_3)
+    @item_1 = create(:item, merchant_id: @merchant_1.id, unit_price: 100)
+    @item_2 = create(:item, merchant_id: @merchant_2.id, unit_price: 200)
+    @item_3 = create(:item, merchant_id: @merchant_3.id, unit_price: 300)
 
-    @item_1 = create(:item, merchant: @merchant_1)
-    @item_2 = create(:item, merchant: @merchant_2)
-    @item_3 = create(:item, merchant: @merchant_3)
+    @invoice_1 = create(:invoice, merchant_id: @merchant_1.id, customer_id: @customer_1.id)
+    @invoice_item_1 = create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: 100)
+    @invoice_item_2 = create(:invoice_item, item_id: @item_2.id, invoice_id: @invoice_1.id, quantity: 5, unit_price: 200)
+    @invoice_item_3 = create(:invoice_item, item_id: @item_3.id, invoice_id: @invoice_1.id, quantity: 10, unit_price: 300)
+    @transaction_1 = create(:transaction, invoice_id: @invoice_1.id)
+    @transaction_2 = create(:transaction, invoice_id: @invoice_1.id)
 
-    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 4)
-    @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 8)
-    @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_3, quantity: 2)
 
-    @transaction_1 = create(:transaction, invoice: @invoice_1)
-    @transaction_2 = create(:transaction, invoice: @invoice_2)
-    @transaction_3 = create(:transaction, invoice: @invoice_3)
+    @invoice_2 = create(:invoice, merchant_id: @merchant_2.id, customer_id: @customer_2.id)
+    @invoice_item_4 = create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: 100)
+    @invoice_item_5 = create(:invoice_item, item_id: @item_2.id, invoice_id: @invoice_2.id, quantity: 5, unit_price: 200)
+    @invoice_item_6 = create(:invoice_item, item_id: @item_3.id, invoice_id: @invoice_2.id, quantity: 10, unit_price: 300)
+    @transaction_3 = create(:transaction, invoice_id: @invoice_2.id)
+    @transaction_4 = create(:transaction, invoice_id: @invoice_2.id)
+
+    @invoice_3 = create(:invoice, merchant_id: @merchant_3.id, customer_id: @customer_3.id)
+    @invoice_item_7 = create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice_3.id, quantity: 1, unit_price: 100)
+    @invoice_item_8 = create(:invoice_item, item_id: @item_2.id, invoice_id: @invoice_3.id, quantity: 5, unit_price: 200)
+    @transaction_5 = create(:transaction, invoice_id: @invoice_3.id)
+    @transaction_6 = create(:transaction, invoice_id: @invoice_3.id)
   end
 
   it "sends a list of merchants" do
@@ -88,15 +99,62 @@ describe "Merchants API" do
     expect(merchant["attributes"]["id"]).to eq(merchant_1.id)
   end
 
-  # it "can return top merchants with most revenue" do
-  #   number = 3
-  #
-  #   get "/api/v1/merchants/most_revenue?quantity=#{number}"
+  # it "can find all merchants by id" do
+  #   get "/api/v1/merchants/find_all?id=#{@merchant_1.id}"
   #
   #   merchants = JSON.parse(response.body)["data"]
   #
   #   expect(response).to be_successful
-  #   expect(merchants.most_revenue.first).to eq(@merchant_1)
-  #   expect(merchants.most_revenue.last).to eq(@merchant_3)
+  #   expect(merchants.count).to eq(3)
+  #   expect(merchants.first["id"].to_i).to eq(@merchant_1.id)
   # end
+
+  # it "can find all merchants by name" do
+  #   get "/api/v1/merchants/find_all?name=#{@merchant_1.name}"
+  #
+  #   merchants = JSON.parse(response.body)["data"]
+  #
+  #   expect(response).to be_successful
+      #expect(merchants.count).to eq(3)
+  #   expect(merchants.first["attributes"]["name"]).to eq(@merchant_1.name)
+  # end
+  #
+  # it "can find first merchant by created_at" do
+  #   date = "2012-03-27 14:54:00 UTC"
+  #   merchant_1 = create(:merchant, created_at: date)
+  #
+  #   get "/api/v1/merchants/find_all?created_at=#{date}"
+  #
+  #   merchants = JSON.parse(response.body)["data"]
+  #
+  #   expect(response).to be_successful
+      #expect(merchants.count).to eq(3)
+  #   expect(merchants.first["attributes"]["id"]).to eq(merchant_1.id)
+  # end
+  #
+  # it "can find first merchant by updated_at" do
+  #   date = "2012-03-27 14:54:00 UTC"
+  #   merchant_1 = create(:merchant, updated_at: date)
+  #
+  #   get "/api/v1/merchants/find_all?updated_at=#{date}"
+  #
+  #   merchants = JSON.parse(response.body)["data"]
+  #
+  #   expect(response).to be_successful
+      #expect(merchants.count).to eq(3)
+  #   expect(merchants.first["attributes"]["id"]).to eq(merchant_1.id)
+  # end
+
+  it "can return top merchants with most revenue" do
+    number = 2
+
+    get "/api/v1/merchants/most_revenue?quantity=#{number}"
+
+    merchants = JSON.parse(response.body)["data"]
+
+    expect(response).to be_successful
+    expect(merchants.count).to eq(number)
+    expect(merchants.first["id"].to_i).to eq(@merchant_1.id)
+    expect(merchants.last["id"].to_i).to eq(@merchant_2.id)
+  end
 end
