1034283f5c32e118abd549a8cedf82d05db53af8
Justin Mauldin <justin.mauldin7@gmail.com>
2019-01-27 15:40:49 -0700

    Adds if statement to show method in merchants revenue controller to account for date param being passed.

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -5,8 +5,14 @@ class Api::V1::Merchants::RevenueController < ApplicationController
   end
 
   def show
-    merchant = Merchant.find(params[:id])
-    revenue = merchant.revenue
-    render json: TotalRevenueSerializer.new(TotalRevenue.new(revenue))
+    if params[:date]
+      merchant = Merchant.find(params[:id])
+      revenue = merchant.single_revenue_by_day(params[:date])
+      render json: TotalRevenueSerializer.new(TotalRevenue.new(revenue))
+    else
+      merchant = Merchant.find(params[:id])
+      revenue = merchant.revenue
+      render json: TotalRevenueSerializer.new(TotalRevenue.new(revenue))
+    end
   end
 end
