e62e44bf07b7e107050a6556578a8c4a0b7525e9
Justin Mauldin <justin.mauldin7@gmail.com>
2019-01-26 22:37:36 -0700

    Adds serializer for the TotalRevenue class.

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def index
-#require "pry", binding.pry
     revenue = Merchant.revenue_by_day(params[:date])
     render json: TotalRevenueSerializer.new(TotalRevenue.new(revenue))
   end
