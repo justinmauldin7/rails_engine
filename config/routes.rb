6a8dc7ecd474d5223a65ce2f4dc9fd6c46d311f7
Justin Mauldin <justin.mauldin7@gmail.com>
2019-01-26 22:01:45 -0700

    Adds route for revenue on a merchant.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
         get 'find_all', to: "search#index"
         get 'most_revenue', to: "most_revenue#index"
         get 'most_items', to: "most_items#index"
+        get 'revenue', to: "revenue#index"
       end
 
       resources :customers, only: [:index, :show]
